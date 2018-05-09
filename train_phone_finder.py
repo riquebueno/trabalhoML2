c9499daaff90ad5830b416fa099ce903196bf0f4
Henrique Bueno <riquebueno@gmail.com>
2018-05-08 21:59:23 -0300

    Add files via upload

---
+++ train_phone_finder.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  8 21:49:00 2018
+
+@author: henriquebueno
+"""
+
+#train_phone_finder.py folder
+
+print("----------------------------FUNFOU! #train_phone_finder.py folder")
