767e92448cf45f5237eda9c615786bd1688102af
Henrique Bueno <riquebueno@gmail.com>
2018-05-09 11:00:12 -0300

    Add files via upload

--- find_phone.py
+++ find_phone.py
@@ -1,11 +1,46 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May  8 21:49:59 2018
+#vou fazer um classificador. Para isso vou quebrar a imagem em vários pedaços
+#a saida de cada imagem sera um vetor onde apenas uma posicao contera o valor 1
+#e todas as demais posicoes conterao o valor 0
 
-@author: henriquebueno
-"""
+from __future__ import absolute_import, division, print_function
+import tensorflow as tf
+import numpy as np
 
-#find_phone.py image
+print("Tensor Flow version: " + str(tf.__version__))
+print("Numpy version: " + str(np.version.version))
 
-print("----------------------------FUNFOU! #find_phone.py image")
+#estou adicionando o grafo gerado no treinamento ao grafo default
+saver = tf.train.import_meta_graph("/tmp/my_model_final_henrique_bueno.ckpt.meta")
+
+#Inicialização da sessão e execução do treinamento
+with tf.Session() as sess:
+    
+    #restaurando o modelo criado pelo script train_phone_finder.py
+    saver.restore(sess, "/tmp/my_model_final_henrique_bueno.ckpt")
+    
+    x1 = tf.get_default_graph().get_tensor_by_name('x:0')
+    sess.run(x1)
+    print(x1)
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
--- train_phone_finder.py
+++ train_phone_finder.py
@@ -1,11 +1,68 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May  8 21:49:00 2018
+#vou fazer um classificador. Para isso vou quebrar a imagem em vários pedaços
+#a saida de cada imagem sera um vetor onde apenas uma posicao contera o valor 1
+#e todas as demais posicoes conterao o valor 0
 
-@author: henriquebueno
-"""
+from __future__ import absolute_import, division, print_function
+import tensorflow as tf
+import numpy as np
 
-#train_phone_finder.py folder
+print("Tensor Flow version: " + str(tf.__version__))
+print("Numpy version: " + str(np.version.version))
 
-print("----------------------------FUNFOU! #train_phone_finder.py folder")
+#Definição das variáveis fora do Tensor Flow
+n_epochs = 300
+
+#Definição de todos os nós do grafo Tensor Flow
+x = tf.Variable(3, name="x")
+y = tf.Variable(4, name="y")
+f = x*x*y + y + 2
+
+#Inicialização de todos os nós do grafo Tensor Flow
+init = tf.global_variables_initializer()
+
+#Criação do saver para persistir o modelo criado em disco
+saver = tf.train.Saver()
+
+#Inicialização da sessão e execução do treinamento
+with tf.Session() as sess:
+    sess.run(init)
+    
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:#checkpoint every 100 epochs
+            #persistindo o modelo criado até o momento
+            save_path = saver.save(sess, "/tmp/my_model_henrique_bueno.ckpt")
+            #save_path = saver.save(sess, "./my_model_henrique_bueno.ckpt")
+        
+        #sess.run(training_op)
+        print("epoch: " + str(epoch))
+        
+    valor_f = f.eval()
+    print("f= " + str(valor_f))
+    
+    #persistindo a versão final do modelo criado
+    save_path = saver.save(sess, "/tmp/my_model_final_henrique_bueno.ckpt")
+    #save_path = saver.save(sess, "./my_model_final_henrique_bueno.ckpt")
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
